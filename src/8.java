a6e1ff2ee13bfe553f54e12c5cb750a42f517258
kipercolletta576 <kipercolletta576@gmail.com>
2025-03-13 22:30:16 +0000

    Add 8.java

---
+++ src/8.java
@@ -0,0 +1,8 @@
+import java.util.*; 
+public class MathHomework {
+    public static void main(String[] args) {
+        // Print a random number between 1 and 10
+        System.out.println(new Random().nextInt(10));
+        
+    }
+}
