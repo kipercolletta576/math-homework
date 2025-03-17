89eac9ab9edab417358c529972971170cf13304c
kipercolletta576 <kipercolletta576@gmail.com>
2025-03-17 18:03:04 +0000

    Add 12.java

---
+++ src/12.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class MathHomework {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int f = sc.nextInt();
+		int g = sc.nextInt();
+		int h = sc.nextInt();
+		int i = sc.nextInt();
+		int j = sc.nextInt();
+		System.out.println(a + b + c + d + e + f + g + h + i + j);
+	}
+}
