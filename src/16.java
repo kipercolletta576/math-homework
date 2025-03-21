edf7d3674c7086360c4fed7f330108f9b5e99fda
kipercolletta576 <kipercolletta576@gmail.com>
2025-03-21 06:49:27 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of basic mathematical operations
+        int sum = 5 + 3;
+        System.out.println("The sum is: " + sum);
+
+        double product = 7 * 4.5;
+        System.out.println("The product is: " + product);
+    }
+}
