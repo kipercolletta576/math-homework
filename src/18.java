ff5f0afe2595fbe7d586e709eddb0c9f74a373fe
kipercolletta576 <kipercolletta576@gmail.com>
2025-03-23 09:36:24 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example Java code to demonstrate basic arithmetic operations
+
+        int sum = 5 + 3;
+        System.out.println("The sum of 5 and 3 is: " + sum);
+        
+        int subtraction = 10 - 2;
+        System.out.println("The difference between 10 and 2 is: " + subtraction);
+    }
+}
