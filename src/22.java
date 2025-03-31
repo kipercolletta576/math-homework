88bfbec982cd1298e23085abd4345915d3edd7cd
kipercolletta576 <kipercolletta576@gmail.com>
2025-03-31 16:53:24 +0000

    Add 22.java

---
+++ src/22.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        double a = 3.14;
+        float b = 2.7f;
+        int c = 5;
+        boolean d = true;
+
+        // Your code here
+    }
+}
