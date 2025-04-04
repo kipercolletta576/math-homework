46076d1d6ee2b1f22a48f998d71bc3ebc45f4a5f
kipercolletta576 <kipercolletta576@gmail.com>
2025-04-04 05:35:00 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,23 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Addition
+        int a = 5;
+        int b = 3;
+        System.out.println("The sum is: " + (a + b));
+
+        // Example 2: Subtraction
+        int c = 8;
+        int d = 4;
+        System.out.println("The difference is: " + (c - d));
+
+        // Example 3: Multiplication
+        int e = 7;
+        int f = 2;
+        System.out.println("The product is: " + (e * f));
+
+        // Example 4: Division
+        int g = 10;
+        int h = 5;
+        System.out.println("The quotient is: " + (g / h));
+    }
+}
