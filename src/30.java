955332754e373932bb03347163e14ae8f6221000
kipercolletta576 <kipercolletta576@gmail.com>
2025-04-09 15:53:06 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,14 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code to calculate simple arithmetic operations
+        int addition = 5 + 3;
+        int subtraction = 10 - 2;
+        int multiplication = 4 * 6;
+        int division = 8 / 2;
+
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+    }
+}
