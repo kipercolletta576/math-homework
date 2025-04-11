a983ce169beaff2ca925a338984a5f6f5a05cf1b
kipercolletta576 <kipercolletta576@gmail.com>
2025-04-11 11:28:19 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of using Java to solve mathematical problems
+        double number1 = 3.0;
+        double number2 = -2.5;
+        
+        double result = (number1 + number2) * 10 / number1;
+        System.out.println("The result is: " + result);
+    }
+}
