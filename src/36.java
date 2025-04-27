3a67c3879f99e9921546dfed494d61c454354e91
kipercolletta576 <kipercolletta576@gmail.com>
2025-04-27 12:40:49 +0000

    Add 36.java

---
+++ src/36.java
@@ -0,0 +1,20 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code for simple arithmetic operations: addition, subtraction, multiplication, division
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2; // Sum of num1 and num2
+        int difference = num1 - num2; // Difference between num1 and num2
+        int product = num1 * num2; // Product of num1 and num2
+        int quotient = num1 / num2; // Quotient of num1 and num2
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+
+        if (num1 > 0) {
+            System.out.println("Num1 is positive.");
+        }
+    }
+}
