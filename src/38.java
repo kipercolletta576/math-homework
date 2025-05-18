3173bc3569c24413d585328ff40f3d23d81933a2
kipercolletta576 <kipercolletta576@gmail.com>
2025-05-18 04:43:58 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example mathematical calculation and logging
+        double result = 5 + 3;
+        System.out.println("Result: " + result);
+        if (result > 10) {
+            System.out.println("The result is greater than 10.");
+        }
+    }
+}
