219849532a1f16fd7379c67520b98a0ce7f51f11
kipercolletta576 <kipercolletta576@gmail.com>
2025-05-19 10:51:04 +0000

    Add 39.java

---
+++ src/39.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example Java program to perform basic arithmetic operations
+        double num1 = 5.0;
+        double num2 = 3.0;
+        double result = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
