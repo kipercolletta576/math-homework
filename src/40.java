9c8af3441f09b1cc6375b112362e30d6a5174e8e
kipercolletta576 <kipercolletta576@gmail.com>
2025-05-23 04:57:45 +0000

    Add 40.java

---
+++ src/40.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Code for basic arithmetic operations
+        int a = 10;
+        int b = 5;
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Quotient: " + (a / b));
+    }
+}
